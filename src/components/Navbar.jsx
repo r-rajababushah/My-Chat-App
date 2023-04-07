3790b942c53a6933018fdd462d39f1b6aeceb346
RajababuShah <rajababushah.in@gmail.com>
2023-04-07 13:24:51 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
   return (
     <div className="navbar">
-      <span className="logo">Portal</span>
+      <span className="logo">Chat App</span>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
